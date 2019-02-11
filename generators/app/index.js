578b82c99b693741d771e47dccbea2e190897798
Julien <julien.courtial07@gmail.com>
2019-02-11 15:33:23 +0100

    fixing bug with mariadb

--- generators/app/index.js
+++ generators/app/index.js
@@ -143,13 +143,13 @@ module.exports = class extends BaseGenerator {
                 jhipsterUtils.rewriteFile({
                     file: jhipsterConstants.DOCKER_DIR + 'backup-mariadb.yml',
                     needle: 'volumes:',
-                    splicable: [`- DB_SERVER=${appName}`]
+                    splicable: [` - DB_SERVER=${appName}`]
                 }, this);
 
                 jhipsterUtils.rewriteFile({
                     file: jhipsterConstants.DOCKER_DIR + 'backup-mariadb.yml',
                     needle: 'volumes:',
-                    splicable: [`- DB_DUMP_CRON=${this.message}`]
+                    splicable: [` - DB_DUMP_CRON=${this.message}`]
                 }, this);
 
                 break;
