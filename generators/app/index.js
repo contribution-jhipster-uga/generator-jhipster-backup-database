e7437ea49958fdc9f1fe3b14b6401df7c2eb8941
drfred1981 <frederic.duvert@laposte.net>
2020-05-09 22:38:05 +0200

    Update index.js
    Fix for new Api Compliance

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,7 +14,7 @@ module.exports = class extends BaseGenerator {
                 }
             },
             readConfig() {
-                this.jhipsterAppConfig = this.getJhipsterAppConfig();
+                this.jhipsterAppConfig = this.getAllJhipsterConfig();
                 if (!this.jhipsterAppConfig) {
                     this.error('Can\'t read .yo-rc.json');
                 }
